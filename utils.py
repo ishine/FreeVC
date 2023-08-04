5b926879880e183c6e0acc08b8fb55b84e8611b7
Lucas Hideki Ueda <lucashueda@gmail.com>
2023-08-04 00:41:22 -0300

    minor fix

--- utils.py
+++ utils.py
@@ -552,6 +552,24 @@ def get_logger(model_dir, filename="train.log"):
   return logger
 
 
+def repeat_expand_2d(content, target_len):
+    # content : [h, t]
+
+    src_len = content.shape[-1]
+    target = torch.zeros([content.shape[0], target_len], dtype=torch.float).to(content.device)
+    temp = torch.arange(src_len+1) * target_len / src_len
+    current_pos = 0
+    for i in range(target_len):
+        if i < temp[current_pos+1]:
+            target[:, i] = content[:, current_pos]
+        else:
+            current_pos += 1
+            target[:, i] = content[:, current_pos]
+
+    return target
+
+
+
 class HParams():
   def __init__(self, **kwargs):
     for k, v in kwargs.items():
